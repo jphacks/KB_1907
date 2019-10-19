ce04258bf61d0f1a00a571c3d0668dfeff99e9ce
odmishien <tmtm.yagi.007@gmail.com>
2019-10-19 11:51:12 +0900

    upload完成

--- app/app.py
+++ app/app.py
@@ -1,22 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import datetime
+import os
+
+ALLOWED_EXTENSIONS = ['mp3', 'flac', 'wav']
+UPLOAD_DIR = 'audio_logs'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    render_template("index.html")
+    return render_template("index.html")
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    i
+    audio = request.files['audio']
+    if audio.filename == '':
+        return redirect('/')
+
+    if allowed_file(audio.filename):
+        saveFileName = datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.' + audio.filename.rsplit('.', 1)[1].lower()
+        audio.save(os.path.join(UPLOAD_DIR, saveFileName))
+        return 'file saved!'
+    else:
+        return redirect('/')
 
 @app.route('/log/<int:log_id>')
 def log(log_id):
-    render_template("log.html")
+    return render_template("log.html")
 
 @app.route('/overview')
 def overview():
-    render_template("overview.html")
+    return render_template("overview.html")
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
