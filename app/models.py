52e6b1777fd5bb4285044b50252a9f36b35a0cb9
odmishien <tmtm.yagi.007@gmail.com>
2019-11-10 01:30:46 +0900

    モデル作り太郎

--- app/models.py
+++ app/models.py
@@ -1,4 +1,38 @@
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, Unicode, UnicodeText, ForeignKey
+from sqlalchemy import Column, Integer, Unicode, DATETIME, JSON, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Unicode(20), unique=True, nullable=False)
+    password = Column(Unicode(20), nullable=False)
+    registerd_at = Column(DATETIME, default=datetime.now, nullable=False)
+
+    def __init__(name, password, registerd_at):
+        self.name = name.title()
+        self.password = password.title()
+        self.registerd_at = registerd_at.title()
+
+class log(db.Model):
+    __tablename__ = "logs"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    topic = Column(JSON, nullable=False)
+    posession = Column(JSON, nullable=False)
+    active_rate = Column(Integer(3), nullable=False)
+    score = Column(JSON, nullable=False)
+    total_time = Column(Integer(10), nullable=False)
+    created_at = = Column(DATETIME, default=datetime.now, nullable=False)
+    
+    def __init__(user_id, topic, posession, active_rate, score, total_time, created_at):
+        self.user_id = user_id.title()
+        self.topic = topic.title()
+        self.posession = posession.title()
+        self.active_rate = active_rate.title()
+        self.score = score.title()
+        self.total_time = total_time.title()
+        self.created_at = created_at.title()
